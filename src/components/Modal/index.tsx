c652914e8792eacb8d07e5b2bce61651011599ae
mateuscaldas <mateus.caldas@hotmail.com>
2022-10-30 19:55:54 +0000

    final

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,51 @@
+import { Component, ReactNode, useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen(): void;
+  children: ReactNode;
+}
+
+interface ModalState {
+  modalStatus: boolean;
+}
+
+export function Modal( {isOpen, setIsOpen, children }: ModalProps ) {
+  const [modalStatus, setModalStatus] = useState<ModalState>({ modalStatus: isOpen });
+ 
+  useEffect(() => {
+    setModalStatus({ modalStatus: isOpen });
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus.modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
